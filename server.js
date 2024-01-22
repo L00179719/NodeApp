a15510dd321fdcde324f46079547df23d0074b44
Adrian Iosif <146378579+L00179719@users.noreply.github.com>
2024-01-22 13:16:31 +0000

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 // Route for the root URL
 app.get('/', (req, res) => {
-    res.status(200).send('Welcom to DevOps App!. Testing the CI/CD pipeline.');
+    res.status(200).send('Welcome!. Testing the CI/CD pipeline.');
 });
 
 
